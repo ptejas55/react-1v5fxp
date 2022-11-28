abec912c199ed5706f059bba47ed272abcf2b2b0
ptejas55 <108654941+ptejas55@users.noreply.github.com>
2022-11-29 00:57:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1v5fxp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1v5fxp)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <p>Hello world!</p>
+      <p>Welcome to User!!</p>
+    </>
+  );
+}
+
+export default App;
